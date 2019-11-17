abe71a2fd10c5d08b3846b355657494b0fec1187
Billy Su <g4691821@gmail.com>
2019-11-17 21:09:47 +0800

    Add Image admin model

--- image_anncs/admin.py
+++ image_anncs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Image
 
 # Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    model = Image
+admin.site.register(Image, ImageAdmin)
